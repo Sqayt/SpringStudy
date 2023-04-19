db6fb5120d477e551e920fbb2d1f131442b2f1ce
SqGoliaf <ivanov@v-1336.ru>
2023-04-19 14:27:02 +0300

    Start study spring

---
+++ EurekaClient/src/main/java/ru/ivanovds/EurekaClient/controllers/MainController.java
@@ -0,0 +1,26 @@
+package ru.ivanovds.EurekaClient.controllers;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String name;
+
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return String.format(
+                "Hello from '%s'", eurekaClient.getApplication(name).getName()
+        );
+    }
+}
